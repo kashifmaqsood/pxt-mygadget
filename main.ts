27b316ae8faf8f89fc1c862da7a4bfcf81d519b5
kashifmaqsood <44828110+kashifmaqsood@users.noreply.github.com>
2021-02-12 11:06:24 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ enum FlyOptions {
     Left,
     Right,
     Forward,
-    Backward,
+    Back,
     Up,
     Down,
 }
@@ -10,28 +10,17 @@ enum FlyOptions {
 enum FlipOptions {
     Left,
     Right,
-    Front,
+    Forward,
     Back
 }
 
 enum TurnOptions {
-    Left,
-    Right
+    Counterclockwise,
+    Clockwise
 }
 
-enum ClawOptions {
-    Open,
-    Close
-}
-
-enum LightOptions {
-    Fixed,
-    Blinking,
-    Oscillating
-}
-
-//% color="#000099" weight=10 icon="\uf17b" block="Mambo"
-namespace Mambo {
+//% color="#000099" weight=10 icon="\uf17b" block="Tello"
+namespace Tello {
 
     //% block color="#006600"
     export function takeoff() {
@@ -42,71 +31,54 @@ namespace Mambo {
     export function land() {
         serial.writeLine("land")
     }
+	
+	//% block color="#006600"
+    export function emergency() {
+        serial.writeLine("emergency")
+    }
 
-    //% block="fly $choice at speed = $x" color="#000099"
-    //% x.min=0 x.max=100 x.defl=20
+    //% block="Fly $choice for distance = $x cm" color="#000099"
+    //% x.min=20 x.max=500 x.defl=50
     export function fly(choice: FlyOptions, x: number) {
         if (choice == 0)
-            serial.writeLine("left " + x)
+            serial.writeLine("left:" + x)
         else if (choice == 1)
-            serial.writeLine("right " + x)
+            serial.writeLine("right:" + x)
         else if (choice == 2)
-            serial.writeLine("forward " + x)
+            serial.writeLine("forward:" + x)
         else if (choice == 3)
-            serial.writeLine("back " + x)
+            serial.writeLine("back:" + x)
         else if (choice == 4)
-            serial.writeLine("up " + x)
+            serial.writeLine("up:" + x)
         else if (choice == 5)
-            serial.writeLine("down " + x)
+            serial.writeLine("down:" + x)
     }
 
-    //% block="turn $choice at speed = $x" color="#3399FF"
-    //% x.min=0 x.max=100 x.defl=20
+    //% block="Turn $choice $x degrees" color="#3399FF"
+    //% x.min=1 x.max=360 x.defl=90
     export function turn(choice: TurnOptions, x: number) {
         if (choice == 0)
-            serial.writeLine("counterclockwise " + x)
+            serial.writeLine("ccw:" + x)
         else if (choice == 1)
-            serial.writeLine("clockwise " + x)
+            serial.writeLine("cw:" + x)
     }
 
-    //% block color="#66CCFF"
-    export function hover() {
-        serial.writeLine("hover")
-    }
-
-    //% block="flip $choice" color="#FF9933"
+    //% block="Set Speed to $x cm/s" color="#66CCFF"
+    //% x.min=10 x.max=100 x.defl=30
+    export function fly(x: number) {
+        serial.writeLine("set_speed:" + x)
+    }	
+	
+    //% block="Flip $choice" color="#FF9933"
     export function flip(choice: FlipOptions) {
         if (choice == 0)
-            serial.writeString("flip left \r\n")
+            serial.writeLine("flip_left")
         else if (choice == 1)
-            serial.writeString("flip right \r\n")
+            serial.writeLine("flip_right")
         else if (choice == 2)
-            serial.writeString("flip front \r\n")
+            serial.writeLine("flip_forward")
         else if (choice == 3)
-            serial.writeString("flip back \r\n")
-    }
-
-    //% block color="#FF0000"
-    export function gunFire() {
-        serial.writeLine("fire")
-    }
-
-    //% block="claw $choice" color="#339933"
-    export function claw(choice: ClawOptions) {
-        if (choice == 0)
-            serial.writeLine("clawopen")
-        else if (choice == 1)
-            serial.writeLine("clawclose")
-    }
-    
-    //% block="headlights mode = $choice brightness = $x" color="#9842f4"
-    //% x.min=0 x.max=100 x.defl=50
-    export function headlights(choice: LightOptions, x: number) {
-        if (choice == 0)
-            serial.writeLine("lightsfixed " + x)
-        else if (choice == 1)
-            serial.writeLine("lightsblinked")
-        else if (choice == 2)
-            serial.writeLine("lightsoscillated")
+            serial.writeLine("flip_back")
     }
 }
+
